3f3fe59255222ae84eb8e7385735a213af4cc153
Connor Peebles <connorpeebles93@gmail.com>
2018-10-30 01:06:23 +0000

    split the given HTML file into React components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import React, {Component} from 'react';
+import Chatbar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
+
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <Navbar />
+        <MessageList />
+        <Chatbar />
+      </div>
     );
   }
 }
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar">
+        <a href="/" className="navbar-brand">Chatty</a>
+      </nav>
+    );
+  }
+}
+
 export default App;
