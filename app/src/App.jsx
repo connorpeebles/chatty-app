b4d1b7fd71a72143f0ca530204637a8f31d1e71e
Connor Peebles <connorpeebles93@gmail.com>
2018-10-30 20:42:50 +0000

    chatty app now connects to websocket server; also updated id's to be randomly generated 6-character combinations instead of incremented counters

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -7,65 +7,65 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      counter: 8,
       currUser: {name: "Bob"},
       messages: [
         {
           type: "incomingMessage",
           content: "I won't be impressed with technology until I can download food.",
           username: "Anonymous1",
-          id: 1
+          id: 111111
         },
         {
           type: "incomingNotification",
           content: "Anonymous1 changed their name to nomnom",
-          id: 2
+          id: 222222
         },
         {
           type: "incomingMessage",
           content: "I wouldn't want to download Kraft Dinner. I'd be scared of cheese packet loss.",
           username: "Anonymous2",
-          id: 3
+          id: 333333
         },
         {
           type: "incomingMessage",
           content: "...",
           username: "nomnom",
-          id: 4
+          id: 444444
         },
         {
           type: "incomingMessage",
           content: "I'd love to download a fried egg, but I'm afraid encryption would scramble it",
           username: "Anonymous2",
-          id: 5
+          id: 555555
         },
         {
           type: "incomingMessage",
           content: "This isn't funny. You're not funny",
           username: "nomnom",
-          id: 6
+          id: 666666
         },
         {
           type: "incomingNotification",
           content: "Anonymous2 changed their name to NotFunny",
-          id: 7
+          id: 777777
         },
       ]
     }
-    this._addMessage = this._addMessage.bind(this);
   }
 
   componentDidMount() {
-    console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
+    this.socket = new WebSocket("ws://localhost:3001");
+    console.log("Connected to server");
+    // console.log("componentDidMount <App />");
+    // setTimeout(() => {
+      // console.log("Simulating incoming message");
       // Add a new message to the list of messages in the data store
-      const newMessage = {id: 8, type: "incomingMessage", username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
+      // const newMessage = {id: 8, type: "incomingMessage", username: "Michelle", content: "Hello there!"};
+      // const messages = this.state.messages.concat(newMessage)
       // Update the state of the app component.
       // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages})
-    }, 3000);
+      // this.setState({messages: messages})
+    // }, 3000);
   }
 
   render() {
@@ -73,21 +73,34 @@ class App extends Component {
       <div>
         <Navbar />
         <MessageList messages={this.state.messages} />
-        <Chatbar currUser={this.state.currUser} addMessage={this._addMessage} />
+        <Chatbar currUser={this.state.currUser} addMessage={this.addMessage} />
       </div>
     );
   }
 
-  _addMessage(username, content) {
-    const newCounter = this.state.counter + 1;
+  addMessage = (username, content) => {
+    const newId = this.generateRandomString();
     const newMessage = {
-      id: newCounter,
+      id: newId,
       type: "incomingMessage",
       username: username,
       content: content
     };
     const messages = this.state.messages.concat(newMessage);
-    this.setState({counter: newCounter, messages: messages});
+    this.setState({messages: messages});
+  }
+
+  generateRandomString = () => {
+    const chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+    let output = "";
+
+    for (let i = 0; i < 6; i++) {
+      let randNum = Math.floor(Math.random() * 36);
+      let char = chars.substring(randNum, randNum + 1);
+      output += char;
+    }
+
+    return output;
   }
 }
 
