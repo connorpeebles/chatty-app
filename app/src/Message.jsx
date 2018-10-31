ce541360653156f93c7b26e572e05367416e0e4c
Connor Peebles <connorpeebles93@gmail.com>
2018-10-31 20:11:39 +0000

    when user adds text after image, it now starts on a new line

--- app/src/Message.jsx
+++ app/src/Message.jsx
@@ -3,32 +3,14 @@ import React, {Component} from 'react';
 class Message extends Component {
   render() {
     const message = this.props.message;
-
     const words = message.content.split(" ");
     const output = words.map((word) => {
       if ((/\.(gif|jpg|jpeg|tiff|png)$/i).test(word)) {
-        return (<img src={word} />);
+        return (<div><img src={word} /></div>);
       } else {
         return word + " ";
       }
     });
-
-    // const searchForImage = (str) => {
-    //   const words = str.split(" ");
-    //   let output = "";
-    //   for (const word of words) {
-    //     if ((/\.(gif|jpg|jpeg|tiff|png)$/i).test(word)) {
-    //       output = output + `<img src='${word}' >` + " ";
-    //     } else {
-    //       output = output + word + " ";
-    //     }
-    //   }
-    //   return output;
-    // };
-
-    //     const messageElements = this.props.messages.map((message) =>
-    //   <Message message={message} key={message.id} />
-    // );
 
     const messageElement = () => {
       if (message.type === "incomingMessage") {
